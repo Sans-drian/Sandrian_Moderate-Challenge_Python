8329e768631bab00e8e09916d39f8364edec21b2
Sandrian W <91533936+Sans-drian@users.noreply.github.com>
2021-12-14 13:51:22 +0700

    Update ModeratelyChallengeClass.py

--- ModeratelyChallengeClass.py
+++ ModeratelyChallengeClass.py
@@ -1,21 +1,26 @@
 class Person:
+    #initiallizer
     def __init__(self, name, address):
         self.name = name
         self.address = address
-
+    
+    #getter functions
     def getName(self):
         return self.name
 
     def getAddress(self):
         return self.address
 
+    #setter function
     def setAddress(self, address):
         self.address = address
 
+    #string function
     def __str__(self):
         return f"Their name is {self.name}, and their address is {self.address}"
 
 class Student(Person):
+    #initiallizer
     def __init__(self, numCourses = 0, courses = [], grades = []):
         self.numCourses = numCourses
         self.courses = courses
@@ -25,34 +30,41 @@ class Student(Person):
         self.course = course
         self.grade = grade
 
+    #print grades    
     def printGrades(self):
         print (self.grade)
-
+        
+    #calculate the average grade
     def getAverageGrade(self):
         ave = sum(self.grade)/(self.numCourses)
         print(f"The average grade is {ave}")
 
+    #print the student's credentials
     def __str__(self):
         return f"The student's name is {self.name}, and their address is {self.address}"
 
 
 class Teacher(Person):
+    #initiallizer
     def __init__(self, numCourses = 0, courses = []):
         self.numCourses = numCourses
         self.courses = courses
 
+    #function for adding the course; checks whether input is the same in the list of courses or not, if yes, print out the text, if no, append.
     def addCourse(self, courses):
         if courses not in self.courses:
             self.course.append(courses)
         else:
             print ("The course you've inputted is already registered.")
 
+    #function for removing a course; checks whether input is the same in the list of courses or not, if yes, remove from the list, if no, print text.
     def removeCourse(self, courses):
         if courses in self.courses:
             self.course.pop(courses)
         else:
             print ("That course does not exist.")
-        
+    
+    #print the teacher's credentials
     def __str__(self):
         return f"The teacher's name is {self.name}, and their address is {self.address}"
 
